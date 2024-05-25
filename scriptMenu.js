26cac34390270f0dfc59749e339cc0b3ec5ef546
ringofdeath <78948459+ringofdeath@users.noreply.github.com>
2024-05-25 11:57:10 -0500

    New Changes for Stack Overflow.

--- scriptMenu.js
+++ scriptMenu.js
@@ -4,6 +4,7 @@ function changeMenu(){
   const parent1 = document.querySelector('.menuSelection');
   activeMenu = parent1.classList.contains('visible');
   hiddenMenu = parent1.classList.contains('hidden');
+  buttonClicked();
   if(hiddenMenu){
     parent1.classList.remove('hidden');
     parent1.classList.add('visible');
@@ -13,6 +14,17 @@ function changeMenu(){
     parent1.classList.add('hidden');
   }
 }
+function buttonClicked(){
+  const button1= document.getElementById('dropdownButton');
+  console.log('Button Clicked Start');
+  for (let index = 0; index < 5; index++) {
+    button1.style.width -= "10px";
+    button1.style.height -= '10px';
+    console.log('Loop');
+  }
+  console.log('Button Function End.');
+}
+console.log('Outside')
 
 const button = document.getElementById('dropdownButton');
 button.addEventListener('click',changeMenu)
